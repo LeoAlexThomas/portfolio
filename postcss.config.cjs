7cd1fb52047737c1669558042d26e0eee2f22c96
LeoAlexThomas <leoalex960@gmail.com>
2024-08-27 13:54:56 +0530

    added mantine style to add style website

---
+++ postcss.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    "postcss-preset-mantine": {},
+    "postcss-simple-vars": {
+      variables: {
+        "mantine-breakpoint-xs": "427px",
+        "mantine-breakpoint-sm": "769px",
+        "mantine-breakpoint-md": "1025px",
+        "mantine-breakpoint-lg": "1441px",
+        "mantine-breakpoint-xl": "2561px",
+      },
+    },
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { MantineProvider } from "@mantine/core";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
 import { theme } from "@/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider theme={theme}>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>
+          <Component {...pageProps} />
+        </MantineProvider>
+      </body>
+    </>
   );
 }
