69f8d812b9b542b10dcfe50d3570c7f8bad4b3ef
LeoAlexThomas <leoalex960@gmail.com>
2024-08-27 15:08:01 +0530

    commented out folder to get style from it when deployed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
@@ -8,15 +8,8 @@ import { theme } from "@/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <MantineProvider theme={theme}>
-          <Component {...pageProps} />
-        </MantineProvider>
-      </body>
-    </>
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
   );
 }
