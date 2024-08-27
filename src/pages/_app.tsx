e58d1b1243bcca551e11bf6b16d2ae27855d319e
LeoAlexThomas <leoalex960@gmail.com>
2024-08-27 16:19:20 +0530

    added firebase deployment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,24 @@ import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { theme } from "@/theme";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPX9zS4XSJarOV7tAK7CWZJywVqVdWnlY",
+  authDomain: "portfolio-e9ae5.firebaseapp.com",
+  projectId: "portfolio-e9ae5",
+  storageBucket: "portfolio-e9ae5.appspot.com",
+  messagingSenderId: "819356773966",
+  appId: "1:819356773966:web:751558a6c3cffc8a8d1d1f",
+};
+
 export default function App({ Component, pageProps }: AppProps) {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
   return (
     <MantineProvider theme={theme}>
       <Component {...pageProps} />
