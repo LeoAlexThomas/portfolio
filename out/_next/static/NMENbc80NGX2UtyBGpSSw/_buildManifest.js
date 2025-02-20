c452ce2db28f6b7e6b305b2afc3dbf1d4737cdd4
LeoAlexThomas <leoalex960@gmail.com>
2025-02-20 17:11:35 +0530

    removed out folder and it's file

--- out/_next/static/NMENbc80NGX2UtyBGpSSw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/361-8eb6a1b98c273e6e.js","static/chunks/pages/index-a715cd4ab6ba897b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
